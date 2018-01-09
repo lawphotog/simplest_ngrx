1f463954bdf13b7673f9478791c2c42f27f63b47
lawphotog <laurence.nyein@gmail.com>
2018-01-09 21:10:17 +0000

    update to effect

--- src/app/effects/photos.effect.ts
+++ src/app/effects/photos.effect.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Effect, Actions } from "@ngrx/effects";
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
 import { PhotoService } from '../services/photos.service';
 import * as fromPhotos from '../actions/photos.action';
+import { map, switchMap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class PhotosEffects {
-    @Effect() loadPhotos$ = this.actions$
+
+    @Effect() 
+    loadPhotos$ = this.actions$
         .ofType(fromPhotos.PhotosActionTypes.LOAD_PHOTOS)
-        .switchMap(() => this.photoService.getPhotos()
-        .map(photos => (new fromPhotos.LoadPhotosSuccessAction(photos)))
+        .pipe(switchMap(() => {
+            return this.photoService
+            .getPhotos()
+            .pipe(
+                map(photos => new fromPhotos.LoadPhotosSuccessAction(photos))
+            )
+        })
     );
 
     constructor(private photoService: PhotoService, private actions$: Actions) {}
